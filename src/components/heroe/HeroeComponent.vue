16c0cc8232e040ec99feb0e89c265119e4943336
carlos mur <cmurestudillos@gmail.com>
2020-10-20 08:41:39 +0200

    commit

--- src/components/heroe/HeroeComponent.vue
+++ src/components/heroe/HeroeComponent.vue
@@ -1,7 +1,47 @@
 <template>
     <div>
-        <h1 class="animated fadeIn slow"><small>Heroe</small></h1>
+        <h1 v-if="heroe.nombre">Heroe: <small>{{heroe.nombre}}</small> </h1>
+        <h1 v-if="!heroe.nombre">Heroe: <small>Nombre del heroe</small> </h1>
         <hr>
+
+        <div class="row text-right">
+            <div class="col">
+                <router-link :to="'/heroes'" class="btn btn-outline-primary" title="Volver"><font-awesome-icon icon="arrow-left"/> Volver </router-link>
+            </div>
+        </div>     
+        <div class="row">
+            <div class="col">
+                <form (ngSubmit)="guardar(formulario)" #formulario="ngForm">
+                <!-- Id del registro -->
+                <div class="form-group">
+                    <label>Id Firebase</label>
+                    <input type="text" class="form-control" placeholder="Id Firebase" [(ngModel)]="heroe.id" name="id" disabled>
+                    <small class="form-text text-muted">Este campo se genera automaticamente.</small>
+                </div>
+                <!-- Nombre -->
+                <div class="form-group">
+                    <label>Nombre</label>
+                    <input type="text" class="form-control" placeholder="Nombre" [(ngModel)]="heroe.nombre" name="nombre" required>
+                </div>
+                <!-- Poder -->
+                <div class="form-group">
+                    <label>Poder</label>
+                    <input type="text" class="form-control" placeholder="Poder" [(ngModel)]="heroe.poder" name="poder">
+                </div>
+                <!-- Estado -->
+                <div class="form-group">
+                    <label>Estado</label>
+                    <br>
+                    <button v-if="heroe.estado" @click="heroe.estado = false" class="btn btn-outline-success w-25" type="button" title="Vivo"><font-awesome-icon icon="thumbs-up" title="Vivo"> Vivo </button>
+                    <button v-if="!heroe.estado" @click="heroe.estado = true" class="btn btn-outline-danger w-25" type="button" title="Muerto"><font-awesome-icon icon="thumbs-down" title="Muerto"> Muerto </button>
+                </div>
+                <hr>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-outline-primary w-25" title="Guardar"><font-awesome-icon icon="save" title="Guardar"> Guardar </button>
+                </div>
+                </form>
+            </div>
+        </div>           
     </div>
 </template>
 
--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEdit, faTrash, faPlus, faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTrash, faPlus, faThumbsUp, faThumbsDown, faArrowLeft, faSave } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
  
-library.add(faEdit, faTrash, faPlus, faThumbsUp, faThumbsDown)
+library.add(faEdit, faTrash, faPlus, faThumbsUp, faThumbsDown, faArrowLeft, faSave)
  
 Vue.component('font-awesome-icon', FontAwesomeIcon)
